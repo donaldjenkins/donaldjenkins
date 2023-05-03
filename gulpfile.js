0044a7b016f1b48ada91003dd1924635ac53b3b0
Donald Jenkins <donaldjenkins@users.noreply.github.com>
2023-05-03 12:08:39 -0400

    Revert "cleaned up gitignore"
    This reverts commit f9fc3355118c01261b56c8a63f217a9bf74d425e.

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
+const minify = require('gulp-clean-css');
+const rename = require('gulp-rename');
+
+gulp.task('compilescss', function() {
+	gulp
+		.src('./assets/scss/*.scss')
+		.pipe(sass())
+		.pipe(prefix())
+		.pipe(minify())
+		.pipe(
+			rename(function(path) {
+				return {
+					dirname: path.dirname + '',
+					basename: path.basename + '.min',
+					extname: '.css',
+				};
+			})
+		)
+		.pipe(gulp.dest('./static/css'));
+});
